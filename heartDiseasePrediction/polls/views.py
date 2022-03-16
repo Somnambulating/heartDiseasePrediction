33a29e0e6ced0d84428842bf10c6f5fb14559084
Oliver <1366033875@qq.com>
2022-03-17 00:09:34 +0800

    1

--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    # template = loader.get_template('polls/index.html')
-    return render(request, 'polls/index.html')
+    template = loader.get_template('polls/index.html')
+    return HttpResponse("hello descosmos")
