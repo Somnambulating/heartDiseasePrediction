49eb873f2d29ff2a26122bdce4d1aabd6d00b344
Oliver <1366033875@qq.com>
2022-03-16 23:32:50 +0800

    remove question

--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Question
-
 
 def index(request):
     template = loader.get_template('polls/index.html')
