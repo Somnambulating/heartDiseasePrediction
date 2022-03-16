a6620b8a52c658efc1ef91fe544a76121ed51972
Oliver <1366033875@qq.com>
2022-03-17 00:36:30 +0800

    path

--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -2,8 +2,9 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from simplejson import load
 
 def index(request):
     context = {}
-    template = loader.get_template('/templates/index.html')
-    return render(request, template, context)
+    # template = loader.get_template('/templates/index.html')
+    return render(request, "index.html", context)
