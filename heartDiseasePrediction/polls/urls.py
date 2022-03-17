1c78db82707d987698d220728ef754fca7642d30
Oliver <1366033875@qq.com>
2022-03-17 10:50:55 +0800

    get

--- heartDiseasePrediction/polls/urls.py
+++ heartDiseasePrediction/polls/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     url(r'^search-post/$', views.search_post)
+# ]
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'^search-post/$', views.search_post)
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators import csrf
 
-def search_post(request):
-    ctx = {}
-    if request.POST:
-        ctx['rlt'] = request.POST['q']
-    return render(request, "index.html", ctx)
-
-def index(request):
-    context = {}
-    context['hello'] = 'Hello World!'
-    return render(request, "index.html", context)
+# 表单
+def search_form(request):
+    return render(request, 'search_form.html')
+ 
+# 接收请求数据
+def search(request):  
+    request.encoding='utf-8'
+    if 'q' in request.GET and request.GET['q']:
+        message = '你搜索的内容为: ' + request.GET['q']
+    else:
+        message = '你提交了空表单'
+    return HttpResponse(message)
