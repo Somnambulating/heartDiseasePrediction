e62f23e73ce5d19570bd3bc34d7da6a7bc7d97c0
Oliver <1366033875@qq.com>
2022-03-17 12:07:57 +0800

    post all

--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -11,22 +11,22 @@ def predict(request):
 
     if request.POST:
         age = request.POST['age']
-        message = message + age
+        sex = request.POST['sex']
+        cp = request.Get['cp']
+        trestbps = request.Get['trestbps']
+        chol = request.Get['chol']
+        fbs	= request.Get['fbs']
+        restecg = request.Get['restecg']
+        thalach = request.Get['thalach']
+        exang = request.Get['exang']
+        oldpeak	= request.Get['oldpeak']
+        slope = request.Get['slope']
+        ca = request.Get['ca']
+        thal = request.Get['thal']
+        message = "result" + thal
     else:
         message = "failed"
-    # sex = request.POST['sex']
-
-    # cp = request.Get['cp']
-    # trestbps = request.Get['trestbps']
-    # chol = request.Get['chol']
-    # fbs	= request.Get['fbs']
-    # restecg = request.Get['restecg']
-    # thalach = request.Get['thalach']
-    # exang = request.Get['exang']
-    # oldpeak	= request.Get['oldpeak']
-    # slope = request.Get['slope']
-    # ca = request.Get['ca']
-    # thal = request.Get['thal']
+    
 
     # message = "without prediction"
 
