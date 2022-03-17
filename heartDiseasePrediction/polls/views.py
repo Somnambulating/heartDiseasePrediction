48576a84631babf0872197efb8fcd880b47be9c3
Oliver <1366033875@qq.com>
2022-03-17 10:44:48 +0800

    post

--- heartDiseasePrediction/polls/views.py
+++ heartDiseasePrediction/polls/views.py
@@ -2,6 +2,13 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators import csrf
+
+def search_post(request):
+    ctx = {}
+    if request.POST:
+        ctx['rlt'] = request.POST['q']
+    return render(request, "index.html", ctx)
 
 def index(request):
     context = {}
