c66f25ca9da27007eb4492c641b152749998941b
Oliver <1366033875@qq.com>
2022-03-18 22:31:13 +0800

    1

--- heartDiseasePrediction/heartDiseasePrediction/settings.py
+++ heartDiseasePrediction/heartDiseasePrediction/settings.py
@@ -119,13 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_DIR = os.path.join(BASE_DIR, 'polls/static/')
-# STATICFILES_DIRS = [
-#     STATIC_DIR
-# ]
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
 
 STATIC_ROOT = STATIC_DIR
-# STATIC_URL = '/static/'
-STATIC_URL = STATIC_DIR
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
