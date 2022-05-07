368a8fb61a2e82cbf779be423d861a8afc3fe7ef
Oliver <1366033875@qq.com>
2022-05-07 19:33:06 +0800

    1

--- heartDiseasePrediction/polls/urls.py
+++ heartDiseasePrediction/polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
